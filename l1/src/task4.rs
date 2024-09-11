4e2658173b2db43951500621ec218a139c690980
nikit <nikitatokin2006@gmail.com>
2024-09-11 19:05:04 +0300

    Изменил тип канала для передачи данных

--- l1/src/task4.rs
+++ l1/src/task4.rs
@@ -1,30 +1,30 @@
-use std::sync::Arc;
-use tokio::sync::{mpsc, Mutex};
+use tokio::sync::broadcast;
 use tokio::time::{self, Duration};
 
-async fn worker(id: usize, rx: Arc<Mutex<mpsc::Receiver<i32>>>) {
+async fn worker(id: usize, mut rx: broadcast::Receiver<i32>) {
     loop {
-        let mut rx = rx.lock().await;
-        if let Some(value) = rx.recv().await {
-            println!("Воркер №{} получил: {}", id, value);
-        } else {
-            break;
+        match rx.recv().await {
+            Ok(value) => {
+                println!("Воркер №{} получил: {}", id, value);
+            }
+            Err(err) => {
+                println!("У воркера №{} приозошла ошибка: {}", id, err);
+            }
         }
     }
 }
 
 async fn task(num_workers: usize) {
-    let (tx, rx) = mpsc::channel(100);
-    let rx = Arc::new(Mutex::new(rx));
+    let (tx, _) = broadcast::channel(100);
     let mut counter = 0;
 
     for id in 1..=num_workers {
-        let worker_rx = rx.clone();
-        tokio::spawn(worker(id, worker_rx));
+        let rx = tx.subscribe();
+        tokio::spawn(worker(id, rx));
     }
 
     loop {
-        if tx.send(counter).await.is_err() {
+        if tx.send(counter).is_err() {
             break;
         }
         println!("Отправлено значение: {}", counter);
@@ -38,9 +38,5 @@ async fn main() {
     let num_workers = 5;
     task(num_workers).await;
 
-
-    // Завершает программу по сигналу
     tokio::signal::ctrl_c().await.unwrap();
-    println!("Завершение программы");
 }
-
