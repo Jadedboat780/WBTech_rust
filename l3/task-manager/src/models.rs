54dc92c65fe49f41987e916989670357ed5da32c
tokin_nikita <nikitatokin2006@gmail.com>
2024-10-09 20:40:04 +0300

    Добавил докер в приложение

--- l3/task-manager/src/models.rs
+++ l3/task-manager/src/models.rs
@@ -3,7 +3,7 @@ use sqlx::{PgPool, postgres::PgQueryResult};
 
 pub type PgResult<T> = Result<T, sqlx::Error>;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, sqlx::FromRow)]
 pub struct Task {
     pub id: i32,
     pub title: String,
@@ -17,20 +17,24 @@ pub struct NewTask {
 
 impl Task {
     pub async fn create(pool: &PgPool, new_task: NewTask) -> PgResult<Task> {
-        let task = sqlx::query_as!(Task, "INSERT INTO tasks (title) VALUES ($1) RETURNING *", new_task.title)
+        let task = sqlx::query_as::<_, Task>("INSERT INTO tasks (title) VALUES ($1) RETURNING *")
+            .bind(new_task.title)
             .fetch_one(pool)
             .await?;
         Ok(task)
     }
 
     pub async fn get(pool: &PgPool, id: i32) -> PgResult<Task> {
-        sqlx::query_as!(Task, "SELECT * FROM tasks WHERE id = $1", id)
+        sqlx::query_as::<_, Task>("SELECT * FROM tasks WHERE id = $1")
+            .bind(id)
             .fetch_one(pool)
             .await
     }
 
     pub async fn complete(pool: &PgPool, id: i32) ->PgResult<PgQueryResult> {
-        sqlx::query!("UPDATE tasks SET completed = $1 WHERE id = $2", true, id)
+        sqlx::query("UPDATE tasks SET completed = $1 WHERE id = $2")
+            .bind(true)
+            .bind(id)
             .execute(pool)
             .await
     }
